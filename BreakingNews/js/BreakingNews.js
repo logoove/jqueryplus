281f7b46f3ca72d954a114ada248f4649065585c
logoove <logove@qq.com>
2018-06-29 00:44:27 +0800

    0

---
+++ BreakingNews/js/BreakingNews.js
@@ -0,0 +1,130 @@
+﻿(function(jQuery){
+
+	$.fn.BreakingNews = function(settings){
+			var defaults={
+					background		:'#FFF',
+					title			:'NEWS',
+					titlecolor		:'#FFF',
+					titlebgcolor	:'#5aa628',
+					linkcolor		:'#333',
+					linkhovercolor	:'#5aa628',
+					fonttextsize	:16,
+					isbold			:false,
+					border			:'none',
+					width			:'100%',
+					autoplay		:true,
+					timer			:3000,
+					modulid			:'brekingnews',
+					effect			:'fade'	//or slide	
+				};
+			var settings=$.extend(defaults,settings);
+			
+			return this.each(function(){
+				settings.modulid="#"+$(this).attr("id");
+				var timername=settings.modulid;
+				var activenewsid=1;
+				
+				if (settings.isbold==true)
+					fontw='bold';
+				else
+					fontw='normal';	
+					
+				if (settings.effect=='slide')
+					$(settings.modulid+' ul li').css({'display':'block'});
+				else
+					$(settings.modulid+' ul li').css({'display':'none'});				
+				
+				$(settings.modulid+' .bn-title').html(settings.title);
+				$(settings.modulid).css({'width':settings.width, 'background':settings.background, 'border':settings.border, 'font-size':settings.fonttextsize });
+				$(settings.modulid+' ul').css({'left':$(settings.modulid+' .bn-title').width()+40});
+				$(settings.modulid+' .bn-title').css({'background':settings.titlebgcolor,'color':settings.titlecolor,'font-weight':fontw});
+				$(settings.modulid+' ul li a').css({'color':settings.linkcolor,'font-weight':fontw,'height':parseInt(settings.fonttextsize)+6});
+				$(settings.modulid+' ul li').eq( parseInt(activenewsid-1) ).css({'display':'block'});
+				
+				// Links hover events ......
+				$(settings.modulid+' ul li a').hover(function() 
+					{
+                    	$(this).css({'color':settings.linkhovercolor});
+					},
+					function ()
+					{
+						$(this).css({'color':settings.linkcolor});
+					}
+				);
+				
+				
+				// Arrows Click Events ......
+				$(settings.modulid+' .bn-arrows span').click(function(e) {
+                    if ( $(this).attr('class')=="bn-arrows-left" )
+						BnAutoPlay('prev');
+					else
+						BnAutoPlay('next');
+                });
+				
+				// Timer events ...............
+				if (settings.autoplay==true)
+				{
+					timername=setInterval(function(){BnAutoPlay('next')},settings.timer);					
+					$(settings.modulid).hover(function()
+						{
+							clearInterval(timername);
+						},
+						function()
+						{
+							timername=setInterval(function(){BnAutoPlay('next')},settings.timer);
+						}
+					);
+				}
+				else
+				{
+					clearInterval(timername);
+				}
+				
+				//timer and click events function ...........
+				function BnAutoPlay(pos)
+				{
+					if ( pos=="next" )
+					{
+						if ( $(settings.modulid+' ul li').length>activenewsid )
+							activenewsid++;
+						else
+							activenewsid=1;
+					}
+					else
+					{
+						if (activenewsid-2==-1)
+							activenewsid=$(settings.modulid+' ul li').length;
+						else
+							activenewsid=activenewsid-1;						
+					}
+					
+					if (settings.effect=='fade')
+					{
+						$(settings.modulid+' ul li').css({'display':'none'});
+						$(settings.modulid+' ul li').eq( parseInt(activenewsid-1) ).fadeIn();
+					}
+					else
+					{
+						$(settings.modulid+' ul').animate({'marginTop':-($(settings.modulid+' ul li').height()+20)*(activenewsid-1)});
+					}
+				}
+				
+				// links size calgulating function ...........
+				$(window).resize(function(e) {
+                    if ( $(settings.modulid).width()<360 )
+					{
+						$(settings.modulid+' .bn-title').html('&nbsp;');
+						$(settings.modulid+' .bn-title').css({ 'width':'4px', 'padding':'10px 0px'});
+						$(settings.modulid+' ul').css({'left':4});
+					}else
+					{
+						$(settings.modulid+' .bn-title').html(settings.title);
+						$(settings.modulid+' .bn-title').css({ 'width':'auto', 'padding':'10px 20px'});
+						$(settings.modulid+' ul').css({'left':$(settings.modulid+' .bn-title').width()+40});
+					}
+                });
+			});
+			
+		};
+		
+})(jQuery);
--- README.md
+++ README.md
@@ -9,3 +9,67 @@
 - php.js 用js写成的php函数,非常适合php开发使用,新增js模板函数tpl;
 - layer 弹出层,功能超过,包括相册看图
 - layui,类似bootstrap,包括了layer所有功能,更加强大
+- slick 幻灯片插件 <http://www.dowebok.com/84.html>
+- wow 页面滚动动画效果依赖animate.min.css,调用方法`new WOW().init();` 在div上加`class="wow slideInLeft"`
+
+配合data-wow-duration（动画持续时间）、data-wow-delay（动画延迟时间）、data-wow-offset（元素的位置露出后距离底部多少像素执行）和data-wow-iteration（动画执行次数）
+~~~
+wow rollIn 	从左到右、顺时针滚动、透明度从100%变化至设定值
+  	 
+wow bounceIn 	从原位置出现，由小变大超出设定值，再变小小于设定值，再回归设定值、透明度从100%变化至设定值
+
+wow bounceInUp 	从下往上、窜上来以后会向上超出一部分然后弹回去、透明度为设定值不变
+
+wow bounceInDown 	从上往下、掉下来以后会向下超出一部分然后弹跳一下、透明度为设定值不变
+
+wow bounceInLeft 	从左往右、移过来以后会向右超出一部分然后往左弹一下、透明度为设定值不变
+
+wow bounceInRight 	从右往左、移过来以后会向左超出一部分然后往右弹一下、透明度为设定值不变
+  	 
+wow slideInUp 	从下往上、上来后固定到设定位置、透明度为设定值不变（up是从下往上）（如果元素在最下面，会撑开盒子高度）
+
+wow slideInDown 	从上往下、上来后固定到设定位置、透明度为设定值不变
+
+wow slideInLeft 	从左往右、上来后固定到设定位置、透明度为设定值不变（left却是从左往右）
+wow slideInRight 	从右往左、上来后固定到设定位置、透明度为设定值不变
+  	 
+wow lightSpeedIn 	从右往左、头部先向右倾斜，又向左倾斜，最后变为原来的形状、透明度从100%变化至设定值
+
+wow pulse 	原位置放大一点点在缩小至原本大小、透明度为设定值不变（配合动画执行次数属性效果更佳）
+
+wow flipInX 	原位置后仰前栽、透明度从100%变化至设定值
+
+wow flipInY 	原位置左右旋动、透明度从100%变化至设定值
+
+wow bounce 	上下抖动、透明度为设定值不变（配合动画执行次数和动画持续时间属性可以实现剧烈抖动亦或是慢慢抖）
+
+wow shake 	左右抖动、透明度为设定值不变（配合动画执行次数和动画持续时间属性可以实现剧烈抖动亦或是慢慢抖）
+
+wow swing 	从右往左、头部先向右倾斜，又向左倾斜，最后变为原来的形状、透明度为设定值不变
+
+wow bounceInU 	原位置不变、直接从不显示到显示（无过过渡效果）
+
+wow wobble 	原位置不变、类似于一个人站在那左右晃头、透明度为设定值不变
+
+~~~
+
+-  top  `<a class="to-top">返回顶部</a> $('.to-top').toTop();`
+autohide 	布尔值 	true 	自动隐藏
+offset 	整数 	420 	距离顶部多少距离时自动隐藏按钮
+speed 	整数 	500 	滚动持续时间
+position 	布尔值 	true 	如果设置为 false，则需要手动在 css 中设置“按钮”的位置
+right 	整数 	15 	右侧距离
+bottom 	整数 	30 	底部距离
+
+
+- thumb 缩略图插件 解决不同大小图片` $('img').jqthumb();`
+classname 		‘jqthumb’ 	缩略图容器的 class
+width 	整数 	100 	缩略图的宽度，单位为 px
+height 	整数 	100 	缩略图的高度，单位为 px
+position 		{top:’50%’, left:’50%’} 	缩略图的位置，默认为 50%，即水平并且垂直居中
+source 	字符串 	‘src’ 	指定图像源属性，默认为 src
+showoncomplete 	布尔值 	true 	处理后时候立即显示，如果为 false 则不自动显示，需要额外设置让它显示，比如使用回调函数 after、done
+before 	函数 		处理前的回调函数
+after 	函数 		某一个图片处理后的回调函数
+done 	函数 		所有图片处理后的回调函数
+
